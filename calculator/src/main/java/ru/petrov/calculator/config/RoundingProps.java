f7b874c3ae59e77bcfa0833ca0854b05f3932493
Aleksei Petrov <peta0982@gmail.com>
2024-05-29 18:53:49 +0500

    fix annotation config class

--- calculator/src/main/java/ru/petrov/calculator/config/RoundingProps.java
+++ calculator/src/main/java/ru/petrov/calculator/config/RoundingProps.java
@@ -2,11 +2,11 @@ package ru.petrov.calculator.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
 import java.math.RoundingMode;
 
-@Component
+@Configuration
 @ConfigurationProperties(prefix="calculator.rounding")
 @Data
 public class RoundingProps {
--- calculator/src/main/java/ru/petrov/calculator/config/ScoringProps.java
+++ calculator/src/main/java/ru/petrov/calculator/config/ScoringProps.java
@@ -2,11 +2,11 @@ package ru.petrov.calculator.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
 import java.math.BigDecimal;
 
-@Component
+@Configuration
 @ConfigurationProperties(prefix="calculator.rates")
 @Data
 public class ScoringProps {
