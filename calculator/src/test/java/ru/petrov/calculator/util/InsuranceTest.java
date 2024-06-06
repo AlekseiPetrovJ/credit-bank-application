e48389aa46e8ceea1a7087c47422106d2b9603dc
Aleksei Petrov <peta0982@gmail.com>
2024-06-06 19:36:11 +0500

    rev. improve InsuranceTest

--- calculator/src/test/java/ru/petrov/calculator/util/InsuranceTest.java
+++ calculator/src/test/java/ru/petrov/calculator/util/InsuranceTest.java
@@ -1,36 +1,24 @@
 package ru.petrov.calculator.util;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.petrov.calculator.config.RoundingProps;
-import ru.petrov.calculator.config.ScoringProps;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class InsuranceTest {
-    @Autowired
-    private ScoringProps scoringProps;
-    @Autowired
-    private RoundingProps roundingProps;
     @Autowired
     private Insurance insurance;
 
-    @ParameterizedTest
-    @ValueSource(ints = {1, 0, 10000, 15555})
+    @Test
     @DisplayName("Получение суммы кредита увеличенной на размер страховки")
-    void getAmountWithInsurance(int amount) {
-        BigDecimal insuranceRate = scoringProps.getInsuranceRate();
-        Integer scale = roundingProps.getScale();
-        RoundingMode roundingMode = roundingProps.getRoundingMode();
-        BigDecimal insExpected = BigDecimal.valueOf(amount).multiply(insuranceRate
-                .divide(BigDecimal.valueOf(100), scale, roundingMode).add(BigDecimal.ONE));
-        assertEquals(insExpected, insurance.getAmountWithInsurance(BigDecimal.valueOf(amount)));
+    void getAmountWithInsurance() {
+        assertEquals(0, insurance.getAmountWithInsurance(BigDecimal.valueOf(1000)).compareTo(BigDecimal.valueOf(2000)));
     }
 }
