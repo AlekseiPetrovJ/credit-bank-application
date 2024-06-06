f872b4067ea3298c85ccbb86cde0953396fe8af3
Aleksei Petrov <peta0982@gmail.com>
2024-06-06 22:42:51 +0500

    rev. moved some methods to parents class; improved method compare json array

--- calculator/src/test/java/ru/petrov/calculator/CalculatorApplicationTests.java
+++ calculator/src/test/java/ru/petrov/calculator/CalculatorApplicationTests.java
@@ -1,13 +1,45 @@
 package ru.petrov.calculator;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 @SpringBootTest
-class CalculatorApplicationTests {
+public class CalculatorApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	public ObjectMapper getObjectMapper() {
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.registerModule(new ParameterNamesModule());
+		objectMapper.registerModule(new JavaTimeModule());
+		return objectMapper;
+	}
+	public Object getObjectFromFile(String path, Class clazz) {
+		try {
+			File file = new ClassPathResource(path).getFile();
+			return getObjectMapper().readValue(file, clazz);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public String getStringFromFile(String path) {
+		try {
+			File file = new ClassPathResource(path).getFile();
+			return new String(Files.readAllBytes(file.toPath()));
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 }
--- calculator/src/test/java/ru/petrov/calculator/service/CalculatorServiceTest.java
+++ calculator/src/test/java/ru/petrov/calculator/service/CalculatorServiceTest.java
@@ -1,32 +1,24 @@
 package ru.petrov.calculator.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
-import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.io.ClassPathResource;
+import ru.petrov.calculator.CalculatorApplicationTests;
 import ru.petrov.calculator.dto.LoanOfferDto;
 import ru.petrov.calculator.dto.LoanStatementRequestDto;
 import ru.petrov.calculator.dto.ScoringDataDto;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-class CalculatorServiceTest {
+class CalculatorServiceTest extends CalculatorApplicationTests {
 
     @Autowired
     private CalculatorService calculatorService;
@@ -40,15 +32,13 @@ class CalculatorServiceTest {
 
     @Test
     @DisplayName("Сверка результата с эталоном")
-    void testPreScoringEquals() throws JSONException {
+    void testPreScoringEquals() throws JSONException, JsonProcessingException {
         List<LoanOfferDto> loanOfferDtos = calculatorService
                 .preScoring((LoanStatementRequestDto) getObjectFromFile("good_LoanStatementRequestDto_1.json",
                         LoanStatementRequestDto.class));
-        String loanOffersActual = getStringFromObject(loanOfferDtos);
-        String loanOffersActualCleared = removeFieldFromJsonArray(loanOffersActual, "statementId");
+        String loanOffersActual = getObjectMapper().writeValueAsString(loanOfferDtos);
         String loanOffersExpected = getStringFromFile("good_list_LoanOfferDto_1.json");
-        String loanOffersExpectedCleared = removeFieldFromJsonArray(loanOffersExpected, "statementId");
-        JSONAssert.assertEquals(loanOffersExpectedCleared, loanOffersActualCleared, JSONCompareMode.LENIENT);
+        JSONAssert.assertEquals(loanOffersExpected, loanOffersActual, JSONCompareMode.LENIENT);
     }
 
     @Test
@@ -69,51 +59,5 @@ class CalculatorServiceTest {
     void testScoringReturnNotNull() {
         ScoringDataDto scoringDataDto = (ScoringDataDto) getObjectFromFile("good_ScoringDataDto_1.json", ScoringDataDto.class);
         assertNotNull(calculatorService.scoring(scoringDataDto));
-    }
-
-    private ObjectMapper getObjectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new ParameterNamesModule());
-        objectMapper.registerModule(new JavaTimeModule());
-        return objectMapper;
-    }
-
-    private Object getObjectFromFile(String path, Class clazz) {
-        try {
-            File file = new ClassPathResource(path).getFile();
-            return getObjectMapper().readValue(file, clazz);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private String getStringFromObject(Object obj) {
-        try {
-            return getObjectMapper().writeValueAsString(obj);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private String getStringFromFile(String path) {
-        try {
-            File file = new ClassPathResource(path).getFile();
-            return new String(Files.readAllBytes(file.toPath()));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private String removeFieldFromJsonArray(String jsonArrayString, String fieldToRemove) {
-        try {
-            JSONArray jsonArray = new JSONArray(jsonArrayString);
-            for (int i = 0; i < jsonArray.length(); i++) {
-                JSONObject jsonObject = jsonArray.getJSONObject(i);
-                jsonObject.remove(fieldToRemove);
-            }
-            return jsonArray.toString();
-        } catch (JSONException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
