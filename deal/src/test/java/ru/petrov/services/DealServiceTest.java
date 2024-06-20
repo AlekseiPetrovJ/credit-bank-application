868ce104fd86235f1ff18176e1efa0d5bd98335e
Aleksei Petrov <peta0982@gmail.com>
2024-06-21 00:29:47 +0500

    rev. test class get object from json

--- deal/src/test/java/ru/petrov/services/DealServiceTest.java
+++ deal/src/test/java/ru/petrov/services/DealServiceTest.java
@@ -1,5 +1,6 @@
 package ru.petrov.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,31 +10,30 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import ru.petrov.DealApplicationTest;
 import ru.petrov.dto.CreditDto;
 import ru.petrov.dto.LoanOfferDto;
 import ru.petrov.dto.LoanStatementRequestDto;
-import ru.petrov.models.*;
+import ru.petrov.models.Client;
+import ru.petrov.models.Credit;
+import ru.petrov.models.LoanOffer;
+import ru.petrov.models.Statement;
 import ru.petrov.models.enums.ApplicationStatus;
 import ru.petrov.models.enums.CreditStatus;
-import ru.petrov.models.enums.MaritalStatus;
 import ru.petrov.repositories.StatementRepository;
 import ru.petrov.util.exceptions.StatementNotFoundException;
 
-import java.math.BigDecimal;
 import java.time.Duration;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-import static ru.petrov.models.enums.EmploymentStatus.UNEMPLOYED;
-import static ru.petrov.models.enums.Position.MID_MANAGER;
 
 @SpringBootTest
 @ActiveProfiles("test")
 @RequiredArgsConstructor
-class DealServiceTest {
+class DealServiceTest extends DealApplicationTest {
     @Autowired
     private StatementRepository statementRepository;
     @Autowired
@@ -47,22 +47,14 @@ class DealServiceTest {
     private LoanStatementRequestDto loanStatementReqDto;
 
     @BeforeEach
-    public void setUp() {
-        client = Client.builder()
-                .email("eeee@sdfsdf.ru")
-                .employment(new Employment(UUID.randomUUID(), UNEMPLOYED, "sdd",
-                        BigDecimal.valueOf(123123), MID_MANAGER, 20, 20))
-                .birthDate(LocalDate.now().minusYears(20))
-                .lastName("Perov")
-                .firstName("Ivan")
-                .middleName("qwe")
-                .maritalStatus(MaritalStatus.DIVORCED).build();
+    public void setUp() throws JsonProcessingException {
+        client = getObjectMapper().readValue(getStringFromFile("good_Client_1.json"),
+                Client.class);
 
         statement = new Statement().builder().client(client).creationDate(LocalDateTime.now()).build();
 
-        loanOfferDto = new LoanOfferDto(null, BigDecimal.valueOf(40000),
-                BigDecimal.valueOf(42000), 10, BigDecimal.valueOf(4200), BigDecimal.valueOf(10),
-                false, false);
+        loanOfferDto = getObjectMapper().readValue(getStringFromFile("good_LoanOfferDto_1.json"),
+                LoanOfferDto.class);
 
         loanStatementReqDto = mapper.map(client, LoanStatementRequestDto.class);
     }
@@ -122,7 +114,7 @@ class DealServiceTest {
                 () -> assertNotNull(updatedStatement),
                 () -> assertThat(savedCredit).usingRecursiveComparison().isEqualTo(updatedStatement.getCredit()),
                 () -> assertEquals(ApplicationStatus.DOCUMENT_CREATED, updatedStatement.getStatus()),
-                () -> assertTrue(updatedStatement.getStatusHistory().size()>0)
+                () -> assertFalse(updatedStatement.getStatusHistory().isEmpty())
         );
     }
 
