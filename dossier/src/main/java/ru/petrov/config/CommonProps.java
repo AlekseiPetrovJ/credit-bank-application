bf55ba30c3725f91fe7cc82149dc445076696f2a
Aleksei Petrov <peta0982@gmail.com>
2024-07-11 18:41:24 +0500

    fix properties naming

--- dossier/src/main/java/ru/petrov/config/CommonProps.java
+++ dossier/src/main/java/ru/petrov/config/CommonProps.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix="dossier.properties")
+@ConfigurationProperties(prefix="dossier")
 @Data
 public class CommonProps {
     private String dealUrl = "http://127.0.0.1:8081";
--- gateway/src/main/java/ru/petrov/config/CommonProps.java
+++ gateway/src/main/java/ru/petrov/config/CommonProps.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix="gateway.properties")
+@ConfigurationProperties(prefix="gateway")
 @Data
 public class CommonProps {
     private String dealUrl = "http://127.0.0.1:8081";
